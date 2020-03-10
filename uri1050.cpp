f99229b22cdddf3cd234b75ec6ef27a7562fb191
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2020-03-10 19:04:32 -0300

    Create uri1050.cpp

---
+++ uri1050.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a;
+	cin >> a;
+	if(a==11 || a==19 || a==21 || a==27 || a==31 || a==32 || a==61 || a==71 ){
+	if(a==11){
+		cout<<"Sao Paulo"<<"\n";
+	}
+	if(a==19){
+		cout<<"Campinas"<<"\n";
+	}
+	if(a==21){
+		cout<<"Rio de Janeiro"<<"\n";
+	}
+	if(a==27){
+		cout<<"Vitoria"<<"\n";
+	}
+	if(a==31){
+		cout<<"Belo Horizonte"<<"\n";
+	}
+	if(a==32){
+		cout<<"Juiz de Fora"<<"\n";
+	}
+	if(a==61){
+		cout<<"Brasilia"<<"\n";
+	}
+	if(a==71){
+		cout<<"Salvador"<<"\n";
+	}
+}
+	else
+		cout<<"DDD nao cadastrado"<<"\n";
+
+}
