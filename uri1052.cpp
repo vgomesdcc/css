a2e2b6726dff4719af2bbaa7cb9c2babe0f1d61f
vgomesdcc <40807939+vgomesdcc@users.noreply.github.com>
2020-03-10 19:05:34 -0300

    Create uri1052.cpp

---
+++ uri1052.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a;
+	cin >> a;
+	if(a==1)
+		cout<<"January"<<"\n";
+	if(a==2)
+		cout<<"February"<<"\n";
+	if(a==3)
+		cout<<"March"<<"\n";
+	if(a==4)
+		cout<<"April"<<"\n";
+	if(a==5)
+		cout<<"May"<<"\n";
+	if(a==6)
+		cout<<"June"<<"\n";
+	if(a==7)
+		cout<<"July"<<"\n";
+	if(a==8)
+		cout<<"August"<<"\n";
+	if(a==9)
+		cout<<"September"<<"\n";
+	if(a==10)
+		cout<<"October"<<"\n";
+	if(a==11)
+		cout<<"November"<<"\n";
+	if(a==12)
+		cout<<"December"<<"\n";
+}
